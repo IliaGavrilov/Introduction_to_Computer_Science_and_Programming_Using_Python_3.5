f63a092e85bbce46f048bb86841bc5f5bed158a6
IliaGavrilov <elijahgavri@gmail.com>
2017-11-01 19:43:55 +0300

    Add files via upload

---
+++ Integer Odd-Even Check.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 13:40:21 2017
+
+@author: Gavrilov
+"""
+
+x = int(input('Enter an integer: ' ))
+if x % 2 == 0:
+    print ('')
+    print ('Even')
+else: 
+    print ('')
+    print ('Odd')
+print ('Done with conditional')
