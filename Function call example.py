146f1741aaca99e675abc78a8761f171a6b55050
IliaGavrilov <elijahgavri@gmail.com>
2017-11-03 12:59:45 +0300

    Add files via upload

---
+++ Function call example.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 18 11:44:21 2017
+
+@author: Gavrilov
+"""
+#I evaluate this sequence of expressions.
+def f (x): #any time I do an invocation of a function, I create a new frame.
+#There is that formal parameter.
+    x = x + 1
+    print("in f(x): x =",x)
+    return x
+x = 3 #So in the global scope, x has a binding to the value of 3.
+z = f(x) #There's the actual parameter I'm going to use,
+#I evaluate that, and in that global frame,
+#I create a set of bindings.
+#First one is evaluating that definition
+#binds the variable name f to something.
