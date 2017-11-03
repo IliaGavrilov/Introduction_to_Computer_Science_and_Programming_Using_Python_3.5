f2241ff2e32bca023c3b32745fca18cffc3431c9
IliaGavrilov <elijahgavri@gmail.com>
2017-11-03 21:18:56 +0300

    Add files via upload

---
+++ Recursion vs Iteration in multiplication.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 11:44:02 2017
+
+@author: Gavrilov
+"""
+#iteration as base for recursion
+def mult_iter(a, b):
+    result = 0
+    while b > 0:
+        b -= 1
+        result += a
+    print(result)    
+    return result
+mult_iter(int(input("Enter a: ")),int(input("Enter b: ")))
+
+def mult_recur(a, b):
+    if b ==1:
+        return a
+    else:
+        return a + mult_recur(a, b-1) #invoking function inside function makes recursion
+print(mult_recur(int(input("Enter a: ")), int(input("Enter b: "))))
