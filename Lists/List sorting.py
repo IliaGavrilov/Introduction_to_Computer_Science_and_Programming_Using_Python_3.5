7496d33a48cdcf63b644582f97e0f74a51da9d5c
IliaGavrilov <elijahgavri@gmail.com>
2017-11-04 23:48:13 +0300

    Add files via upload

---
+++ Lists/List sorting.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  4 23:41:42 2017
+
+@author: Gavrilov
+"""
+
+L=[9,6,0,3]
+print(sorted(L)) #returns sorted list, does not mutate L
+                 #handy if I want to keep L around but I want a sorted version of it
+print(L)
+L.sort() #mutates L
+print(L)
+
+L.reverse() #it will change l itself and create something in reverse order
+print(L)
