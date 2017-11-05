e4fe20d20eff3d2a85cd590b95ea7120d3d2081f
IliaGavrilov <elijahgavri@gmail.com>
2017-11-05 14:19:48 +0300

    Update List sorting.py

--- Lists/List sorting.py
+++ Lists/List sorting.py
@@ -9,7 +9,7 @@ L=[9,6,0,3]
 print(sorted(L)) #returns sorted list, does not mutate L
                  #handy if I want to keep L around but I want a sorted version of it
 print(L)
-L.sort() #mutates L
+L.sort() #mutates L, but doesn't return anything
 print(L)
 
 L.reverse() #it will change l itself and create something in reverse order
