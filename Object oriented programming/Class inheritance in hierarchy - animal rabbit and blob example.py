cf8c29bb826fcd1765126b1a70da93f2d905e7e4
IliaGavrilov <elijahgavri@gmail.com>
2017-11-09 20:00:40 +0300

    Update Class inheritance in hierarchy - animal rabbit and blob example.py

--- Object oriented programming/Class inheritance in hierarchy - animal rabbit and blob example.py
+++ Object oriented programming/Class inheritance in hierarchy - animal rabbit and blob example.py
@@ -33,48 +33,6 @@ class Rabbit(Animal): #children subclass
         print("meep")
     def __str__(self):
         return "Rabbit: " + str(self.name) + ":" + str(self.age) 
-    
-class Person(Animal): #children subclass
-    def __init__(self, name, age):
-        Animal.__init__(self, age)
-        Animal.setName(self, name)
-        self.friends = []
-    def getFriends(self):
-        return self.friends
-    def addFriend(self, fname):
-        if fname not in self.friends:
-            self.friends.append(fname)
-    def speak(self):
-        print ("hello")
-    def ageDiff(self,other):
-        diff = self.getAge() - other.getAge()
-        if self.age > other.age:
-            print(self.name, "is", diff, "years older than", other.name)
-        else:
-            print(self.name, "is", -diff, "years younger than", other.name)
-    def __str__(self):
-        return "person: "+ str(self.name) + ":" + str(self.age)
-
-import random
-
-class Student(Person):
-    def __init__(self, name, age, major=None):
-        Person.__init__(self, name, age)
-        self.major = major
-    def changeMajor(self, major):
-        self.major = major
-    def speak(self):
-        r = random.random()
-        if r < 0.25:
-            print("I have homework")
-        elif 0.25 <= r < 0.5:
-            print("I need sleep")
-        elif 0.5 <= r < 0.75:
-            print("I should eat")
-        else:
-            print("I am watching TV")
-    def __str__(self):
-        return "Student: " + str(self.name) + ":" + str(self.age) + ":" + str(self.major)
 
 jelly = Cat(1)
 jelly.setName("JellyBelly")      
@@ -88,11 +46,3 @@ blob = Animal(3)
 #print(blob.speak()) #AttributeError because there is no speak() method associated with Animal.
                      #Blob is an instance of Animal. Look up an Animal. 
                      #The definition for a method for speak() - there isn't one.
-
-#eric = Person("Eric", 45)
-#john = Person("John", 55)
-#print(eric.ageDiff(john))
-#print(Person.ageDiff(john,eric))
-#fred = Student("Fred", 18, "Course VI")
-#print(fred)
-#print(fred.speak())
